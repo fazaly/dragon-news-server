2ace9b353f4d69e608b94f25faeeb162507b9620
Md. Fazaly Rabby <fazalyrabbi614@gmail.com>
2022-10-21 09:26:24 +0600

    ok

--- index.js
+++ index.js
@@ -7,12 +7,36 @@ app.use(cors());
 
 const categories = require('./Data/categories.json');
 
+const news = require('./Data/news.json');
+
 app.get('/', (req, res) => {
     res.send('News API Running');
 });
 
 app.get('/news-categories', (req, res) => {
     res.send(categories);
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if(id === '08' ){
+        res.send(news);
+    }
+    else{
+        const categoryNews = news.filter( n => n.category_id === id);
+        res.send(categoryNews);
+    }
+})
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find( n => n._id === id);
+    res.send(selectedNews);
+    // console.log(req.params.id);
 })
 
 app.listen(port, () => {
